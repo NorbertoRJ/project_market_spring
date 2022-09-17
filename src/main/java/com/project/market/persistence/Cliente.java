1c5a18531c8453174a906525af042cc814a3e760
Norberto Ruiz <narj@norbertoruiz.com>
2022-09-17 02:00:48 -0500

    Mapeo de tablas a clases

---
+++ src/main/java/com/project/market/persistence/Cliente.java
@@ -0,0 +1,68 @@
+package com.project.market.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private Long celular;
+    private String direcccion;
+    @Column(name = "correo_electronico")
+    private String correElectronico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Number getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDirecccion() {
+        return direcccion;
+    }
+
+    public void setDirecccion(String direcccion) {
+        this.direcccion = direcccion;
+    }
+
+    public String getCorreElectronico() {
+        return correElectronico;
+    }
+
+    public void setCorreElectronico(String correElectronico) {
+        this.correElectronico = correElectronico;
+    }
+}
